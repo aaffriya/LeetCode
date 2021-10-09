622cea31978060769e3b22e2a80396d5994b9cb3
Bhupender Yadav <85007109+aaffriya@users.noreply.github.com>
2021-10-09 19:09:28 +0530

    Create 70. Climbing Stairs.py

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/climbing-stairs/submissions/
+class Solution:
+    def climbStairs(self, n: int) -> int:
+            
+            if n<=3: return n
+            l = [2,3]
+            for i in range(3,n):
+                l.append(l[-1] + l[-2])
+            else:
+                return l[-1]
