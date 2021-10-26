bb05a576a97acf1631369947d8dc8ba2f3f17b83
Bhupender Yadav <85007109+aaffriya@users.noreply.github.com>
2021-10-26 13:23:38 +0530

    Create 977. Squares of a Sorted Array.py

---
+++ Algorithm 1/977. Squares of a Sorted Array.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/squares-of-a-sorted-array/
+# Day 2
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        
+        i, j = 0, len(nums)-1
+        
+        res = [None] * (j + 1)
+        
+        for r in range(j, -1, -1):
+            if abs(nums[i]) > abs(nums[j]):
+                res[r] = nums[i] ** 2
+                i += 1
+                
+            else:
+                res[r] = nums[j] ** 2
+                j -= 1
+        else:
+            return res
