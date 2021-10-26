9cb6371745f1aecdf5db6d76f679e3aa4a8f848a
Bhupender Yadav <85007109+aaffriya@users.noreply.github.com>
2021-10-26 16:12:10 +0530

    Create 746. Min Cost Climbing Stairs.py

---
+++ Dynamic Programming 1/746. Min Cost Climbing Stairs.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/min-cost-climbing-stairs/
+# Day 2
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        for i in range(2, len(cost)):
+            
+            cost[i] += min(cost[i-2], cost[i-1])
+        
+        
+        return min(cost[len(cost)-2], cost[len(cost)-1])
