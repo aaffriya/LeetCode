c0bc524e65d40e84180686ea3ea903670ea28efb
Bhupender Yadav <85007109+aaffriya@users.noreply.github.com>
2021-11-21 09:15:29 +0530

    Create 5201. Watering Plants.py

---
+++ Challenge/5201. Watering Plants.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/contest/weekly-contest-268/problems/watering-plants/
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        
+        step = 0
+        
+        temp = capacity
+        
+        for index, plant in enumerate(plants):
+            if plant <= temp:
+                step += 1
+                temp -= plant
+            else:
+                step += index + index + 1
+                temp = capacity
+                temp -= plant
+                
+        return step
+                
