2d4a81466ba31f0db2e12a8794caa4e2b071a84a
Bhupender Yadav <85007109+aaffriya@users.noreply.github.com>
2021-12-15 16:17:58 +0530

    Create 21. Merge Two Sorted Lists.py

---
+++ 21. Merge Two Sorted Lists.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/merge-two-sorted-lists/
+class Solution:
+    def mergeTwoLists(self, a, b):
+        if a and b:
+            if a.val > b.val:
+                a, b = b, a
+            a.next = self.mergeTwoLists(a.next, b)
+        return a or b
